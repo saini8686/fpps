b05bf4c6c2d1c5bbaad969493330e8055a912021
saini8686 <ks3399094@gmail.com>
2025-03-04 15:33:05 +0530

    mw-portfolio

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const express = require("express");
-const axios = require("axios"); // For making API calls
+const axios = require("axios");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 // Middleware to parse JSON
 app.use(bodyParser.json());
@@ -18,20 +18,22 @@ app.post("/webflow-form", async (req, res) => {
 
     // Map Webflow form fields to ClickUp task fields
     const taskData = {
-      name: `Form Submission: ${formData.data["Name"]}`, // Use the form's "Name" field
-      description: `**Email**: ${formData.data["Email"]}
-      **Phone**: ${formData.data["Phone"]}
-      **Message**: ${formData.data["Message"]}`,
-      status: "to do", // Adjust the status as per your ClickUp workspace
-      assignees: [5496465],
+      name: `Form Submission: ${formData.data["name"]}`, // Use the form's "Name" field
+      description: `**Email**: ${formData.data["email"]}
+      **Phone**: ${formData.data["phone"]}
+      **Company/Business Name**: ${formData.data["company"]}
+      **Add your project goals and requirements here**: ${formData.data["message"]}`,
+      status: "open",
+      // Adjust the status as per your ClickUp workspace
+      assignees: null,
       priority: 2,
       start_date: currentDate,
-      due_date: currentDate,
+      due_date: null,
     };
 
     // ClickUp API details
-    const listId = "901604956254"; // Replace with your ClickUp List ID
-    const apiToken = "pk_55289378_CMDSU7G0B4GOPHB83WVV92JOL73ELHDC"; // Replace with your ClickUp API token
+    const listId = "901601671053"; // Replace with your ClickUp List ID
+    const apiToken = "pk_5431942_F1SRFXKXPU9X1CLG8HAAPBH385TD0TQL"; // Replace with your ClickUp API token
 
     // Send data to ClickUp
     const response = await axios.post(
@@ -44,7 +46,7 @@ app.post("/webflow-form", async (req, res) => {
         },
       }
     );
-
+    console.log(response, "response");
     res
       .status(200)
       .send({ success: true, message: "Task created in ClickUp!" });
@@ -53,9 +55,7 @@ app.post("/webflow-form", async (req, res) => {
       "Error creating task in ClickUp:",
       error.response?.data || error.message
     );
-    res
-      .status(500)
-      .send({ success: false, message: "Error creating task in ClickUp." });
+    res.status(500).send({ success: false, message: req.body });
   }
 });
 
