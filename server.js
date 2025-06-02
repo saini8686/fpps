63b79ab8cb521d32eec8a1db8d9d9d8f964c485d
saini8686 <ks3399094@gmail.com>
2025-06-02 14:29:50 +0530

    some changes

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const PORT = 3000;
 app.use(express.json());
 
 const UPLOADCARE_SECRET_KEY = "b4c69683e25db846bc47";
+const UPLOADCARE_PUB_KEY = "69b27fb42b6f0908baea";
+
 app.post("/uploadcare-signature", (req, res) => {
   const timestamp = Math.floor(Date.now() / 1000) + 60 * 30; // 30 min validity
-  const signatureString = `UPLOADCARE_PUB_KEY=${req.body.pub_key}&expire=${timestamp}`;
+  const signatureString = `UPLOADCARE_PUB_KEY=${UPLOADCARE_PUB_KEY}&expire=${timestamp}`;
   const signature = crypto
     .createHmac("sha256", UPLOADCARE_SECRET_KEY)
     .update(signatureString)
